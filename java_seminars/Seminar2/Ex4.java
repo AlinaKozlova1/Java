156f9e5544660f072a89d5d702ce865c3a4c3baa
Alina <alinakozlova908@gmail.com>
2023-02-02 13:43:36 +0300

    did the forth seminar exercise

--- java_seminars/Seminar2/Ex4.java
+++ java_seminars/Seminar2/Ex4.java
@@ -1,10 +1,29 @@
 package java_seminars.Seminar2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Logger;
+
 // Напишите метод, который составит строку, состоящую из 100 повторений слова TEST и метод, 
 // который запишет эту строку в простой текстовый файл, обработайте исключения.
 public class Ex4 {
     public static void main(String[] arg) {
-        int[] array = new int[] { 513, 21, 33, 5 };
-        System.out.println(array[array.length-1]);
+        String fileName = "PrintTest.txt";
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i<101; i++){
+            sb.append("TEST ");
+            if (i%10 == 0){
+                sb.append("\n");
+            }
+        }
+        try (FileWriter fw = new FileWriter(fileName, true)){
+            String text = sb.toString();
+            fw.write(text);
+        }catch(IOException e){
+            e.printStackTrace();
+            Logger logger = Logger.getAnonymousLogger();
+            logger.warning("We wrote a file");
+        }
+
     }
 }
